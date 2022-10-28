8b227062bb246af9c785f4400d60f7179689cd07
ConnorMcGehee <connorjames050@gmail.com>
2022-10-28 05:06:44 -0500

    Added content & style to index.html

--- scripts.js
+++ scripts.js
@@ -23,10 +23,28 @@ menuButton.addEventListener("click", () => {
 menuButton.addEventListener("mouseover", () => {
 
     menuButton.style.background = "#20c3bc";
+    menuButton.style.cursor = "pointer";
 });
 menuButton.addEventListener("mouseout", () => {
 
+    menuButton.style.cursor = "initial";
     if (!isNavOpen) {
         menuButton.style.background = "#36aca6";
     }
 });
+
+const mediaQuery = window.matchMedia('(min-width: 48em)');
+
+mediaQuery.onchange = (e) => handleScreenChange(e);
+
+const handleScreenChange = (e) => {
+    const nav = $("nav");
+    if (!e.matches) {
+        navList.style.display = "none";
+        isNavOpen = false;
+    } else {
+        navList.style.display = "flex";
+    }
+}
+// Initial check
+handleScreenChange(mediaQuery); 
